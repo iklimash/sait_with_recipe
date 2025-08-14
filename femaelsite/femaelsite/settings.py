3a6adc7a10acaccf936315edecc53c6df18b426b
iklimash <ivklimash@stud.etu.ru>
2025-08-14 11:29:05 +0300

    css

--- femaelsite/femaelsite/settings.py
+++ femaelsite/femaelsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b#-ajp5v73cc_9st4ms(@e=z38%=tbqxay^^b*jv-@@6-)*gnm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
