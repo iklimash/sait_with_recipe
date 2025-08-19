0e0cd1e4d7a326f9b4d62e60b8bb87f39449b745
iklimash <ivklimash@stud.etu.ru>
2025-08-19 11:07:32 +0300

    register

--- femaelsite/users/forms.py
+++ femaelsite/users/forms.py
@@ -1,4 +1,5 @@
-
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django import forms
 
@@ -23,8 +24,40 @@ class LoginUserForm(forms.Form):
         required=True
     )
 
-# class LoginUserForm(forms.Form):
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(
+        label='Имя пользователя',
+        widget=forms.TextInput(attrs={
+            'class': 'form-input',
+            'id': 'username',
+            'placeholder': 'Введите имя пользователя'
+        }),
+        required=True
+    )
+    password1 = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-input',
+            'id': 'password1',
+            'placeholder': 'Введите ваш пароль'
+        }),
+        required=True
+    )
+    password2 = forms.CharField(
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-input',
+            'id': 'password2',
+            'placeholder': 'Повторите ваш пароль'
+        }),
+        required=True
+    )
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'password1', 'password2']
 
-#     username = forms.CharField(label = 'login', widget = forms.TextInput(attrs = {'class': 'form-input'})) 
-#     password = forms.CharField(label = 'login', widget = forms.PasswordInput(attrs = {'class': 'form-input'})) 
-#     pass
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password1'] != cd['password2']:
+            raise forms.ValidationError('Пароли не совпадают!')
+        return cd['password']
--- femaelsite/users/views.py
+++ femaelsite/users/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
+from django.contrib.auth.models import User
 
-
-
-from .forms import LoginUserForm
+from .forms import LoginUserForm, RegisterUserForm
 
 def login_user(request):
     if request.method == 'POST':
@@ -29,3 +28,19 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect(reverse('users:login'))
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit = False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return render(request, 'users/register_done.html')
+    else:
+        form = RegisterUserForm()
+
+    return render(request, 'users/register.html', {
+        'form': form,
+        'title': 'Регистрация'
+    })
